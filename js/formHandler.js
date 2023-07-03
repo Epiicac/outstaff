8863768732f49b2da5228854159f96ca5d4ab2d2
Catlilface <aleklim98@gmail.com>
2023-07-03 15:20:30 +0300

    accordeon mobile

--- js/formHandler.js
+++ js/formHandler.js
@@ -4,14 +4,14 @@ document.querySelector('#contractor-submit').addEventListener('click', (e) => {
     e.preventDefault()
 
     const options = {
+      mode: 'no-cors',
       headers: {
+        'Access-Control-Allow-Origin': '*',
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer 6f0f29b81c8818a40c3e1b3e611b1774'
       }
     }
     fetch('https://api.mailopost.ru/v1/email/templates/847704/messages', options)
-      .then(res => res.json())
-      .then(data => console.log(data))
+      .then(res => console.log(res))
   }
 })
 
